8bc9f94868dc2ffd750b17f56ef8f55148e86183
Parth Chheda <parth.h.chheda@gmail.com>
2025-01-18 14:45:25 -0800

    made place bets page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./index.css";
 import HomePage from "./components/HomePage";
 import Loading from "./components/Loading";
+import PlaceBets from "./components/PlaceBets";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/loading" element={<Loading />} />
+          <Route path="/place-bets" element={<PlaceBets />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/components/PlaceBets.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+function PlaceBets() {
+  const [bettingAmount, setBettingAmount] = useState(0);
+  const [balance, setBalance] = useState(1000);
+
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <h1 className="my-20 font-bold text-5xl">Place your bets!</h1>
+      <div className="flex flex-row justify-evenly items-center">
+        <div className="flex flex-col items-center justify-center mx-6">
+          <button
+            onClick={() => {
+              if (balance - 10 >= 0) {
+                setBettingAmount(bettingAmount + 10);
+                setBalance(balance - 10);
+              }
+            }}
+          >
+            {/* <img src="../assets/pokerchip.png" alt="Poker Chip" /> */}
+            <div className="rounded-full w-16 h-16 bg-red-600 mb-1"></div>
+          </button>
+          <p className="text-2xl">10</p>
+        </div>
+        <div className="flex flex-col items-center justify-center mx-6">
+          <button
+            onClick={() => {
+              if (balance - 25 >= 0) {
+                setBettingAmount(bettingAmount + 25);
+                setBalance(balance - 25);
+              }
+            }}
+          >
+            {/* <img src="../assets/pokerchip.png" alt="Poker Chip" /> */}
+            <div className="rounded-full w-16 h-16 bg-blue-600 mb-1"></div>
+          </button>
+          <p className="text-2xl">25</p>
+        </div>
+        <div className="flex flex-col items-center justify-center mx-6">
+          <button
+            onClick={() => {
+              if (balance - 50 >= 0) {
+                setBettingAmount(bettingAmount + 50);
+                setBalance(balance - 50);
+              }
+            }}
+          >
+            {/* <img src="../assets/pokerchip.png" alt="Poker Chip" /> */}
+            <div className="rounded-full w-16 h-16 bg-green-600 mb-1"></div>
+          </button>
+          <p className="text-2xl">50</p>
+        </div>
+        <div className="flex flex-col items-center justify-center mx-6">
+          <button
+            onClick={() => {
+              if (balance - 100 >= 0) {
+                setBettingAmount(bettingAmount + 100);
+                setBalance(balance - 100);
+              }
+            }}
+          >
+            {/* <img src="../assets/pokerchip.png" alt="Poker Chip" /> */}
+            <div className="rounded-full w-16 h-16 bg-black mb-1"></div>
+          </button>
+          <p className="text-2xl">100</p>
+        </div>
+      </div>
+      <div className="flex flex-col justify-evenly items-center my-20">
+        <p className="font-semibold text-xl">
+          Current Betting Amount: {bettingAmount}
+        </p>
+        <p className="font-semibold text-xl">Balance: {balance}</p>
+        <button
+          className="bg-[#b29588] px-2 py-3 rounded-lg w-52 my-3"
+          onClick={() => {
+            setBalance(1000);
+            setBettingAmount(0);
+          }}
+        >
+          Reset Betting Amount
+        </button>
+        <button className="bg-[#a2b288] px-2 py-3 rounded-lg w-52">
+          Play Game
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PlaceBets;
