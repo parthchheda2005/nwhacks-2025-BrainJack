92f5345f98f6df96af319a7cc9b5e246c083c49b
Parth Chheda <parth.h.chheda@gmail.com>
2025-01-18 13:12:58 -0800

    branch setup

---
+++ frontend/components/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="font-bold text-5xl">Blackjack!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "../components/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
